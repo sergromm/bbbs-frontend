68220938233d28034ba50fe730a97298fe9530c4
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-05-27 17:13:36 +0300

    feat: добавить открытие попапа в ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -3,14 +3,24 @@ import { Route, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-function ProtectedRoute({ component: Component, onSign, onZoomEvent }) {
+function ProtectedRoute({
+  component: Component,
+  onSign,
+  onZoomEvent,
+  onAutoAuth,
+}) {
   const { isLoggedIn } = React.useContext(CurrentUserContext);
+  function protect() {
+    onAutoAuth();
+    return <Redirect to="/" />;
+  }
+
   return (
     <Route>
       {isLoggedIn ? (
         <Component onSign={onSign} onZoomEvent={onZoomEvent} />
       ) : (
-        <Redirect to="/" />
+        protect()
       )}
     </Route>
   );
@@ -20,6 +30,7 @@ ProtectedRoute.propTypes = {
   component: PropTypes.element.isRequired,
   onSign: PropTypes.func.isRequired,
   onZoomEvent: PropTypes.func.isRequired,
+  onAutoAuth: PropTypes.func.isRequired,
 };
 
 export default ProtectedRoute;
