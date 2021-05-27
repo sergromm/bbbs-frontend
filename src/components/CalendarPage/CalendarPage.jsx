99d049980f16954c700944fc335ddebd34b17655
Roman Sergichev <sergichev@gmail.com>
2021-05-27 10:46:53 +0300

    feat: добавить логику фильтрации календаря

--- src/components/CalendarPage/CalendarPage.jsx
+++ src/components/CalendarPage/CalendarPage.jsx
@@ -1,48 +1,81 @@
-import React from "react";
+import { parseISO, format } from "date-fns";
+import { ru } from "date-fns/locale";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import EventCard from "../EventCard/EventCard";
 import api from "../../utils/api/api";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
+import Filter from "../Filter/Filter";
 
 function CalendarPage({ onZoomEvent, onSign }) {
   const { city } = React.useContext(CurrentUserContext);
   const token = localStorage.getItem("access");
-  const [events, setEvents] = React.useState();
+  const [events, setEvents] = React.useState([]);
+  const [filteredEvents, setFilteredEvents] = React.useState([]);
+  const [monthes, setMonthes] = useState([]);
+  const [currentActiveButton, setCurrentActiveButton] = useState("");
+
+  // функция возвращает объект месяца, чтобы добавить его в массив месяцев
+  const getMonth = (event) => {
+    const name = format(parseISO(event.startAt), "LLLL", { locale: ru });
+    const number = Number(format(parseISO(event.startAt), "M"));
+    return { number, name };
+  };
+
+  // получаем массив месяцев в которых будут евенты
+  const getMonthes = (eventList) => {
+    const arr = eventList.map(getMonth);
+    return arr;
+  };
+
+  // При клике на кнопку фильтра фильтруем список месяцев по названию кнопки
+  const handleFilterByMonth = (evt, monthName) => {
+    const month = evt.target.innerText.toLowerCase();
+    if (monthName !== currentActiveButton) {
+      const arrayOfFilteredEvents = events.filter(
+        (event) =>
+          month === format(parseISO(event.startAt), "LLLL", { locale: ru })
+      );
+      setCurrentActiveButton(monthName);
+      setFilteredEvents(arrayOfFilteredEvents);
+    } else {
+      // если нажали на активную кнопку сбрасываем фильтр
+      setCurrentActiveButton("");
+      setFilteredEvents(events);
+    }
+  };
+
   // получаем массив событий при загрузке странице
   React.useEffect(() => {
-    api.getEvents(city, token).then(setEvents).catch(new Error());
+    api
+      .getEvents(city, token)
+      .then((data) => {
+        setEvents(data);
+        setFilteredEvents(data);
+        setMonthes(getMonthes(data));
+      })
+      .catch(new Error());
   }, []);
 
   return (
     <main className="calendar-page">
       <h2 className="title">Календарь</h2>
-      <div className="filter">
-        <button
-          type="button"
-          aria-label="Все"
-          className="filter__button filter__button_active"
-        >
-          Декабрь
-        </button>
-        <button
-          type="button"
-          aria-label="Выбор наставников"
-          className="filter__button"
-        >
-          Январь
-        </button>
-      </div>
+      {monthes && (
+        <Filter
+          currentActiveButton={currentActiveButton}
+          labels={monthes}
+          handleFilter={handleFilterByMonth}
+        />
+      )}
       <section className="calendar__events-grid">
-        {/* рендерим карточку, только если пришел ответ с массивом карточек */}
-        {events &&
-          events.map((event) => (
-            <EventCard
-              key={event.id}
-              event={event}
-              onZoomEvent={onZoomEvent}
-              onSign={onSign}
-            />
-          ))}
+        {filteredEvents.map((event) => (
+          <EventCard
+            key={event.id}
+            event={event}
+            onZoomEvent={onZoomEvent}
+            onSign={onSign}
+          />
+        ))}
       </section>
     </main>
   );
